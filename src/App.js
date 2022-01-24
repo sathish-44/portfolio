51476a26895120ee1f0c970e12c784078ffb8b60
Sathish <sathishmcc94@gmail.com>
2022-01-24 17:15:38 +0530

    theme toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme, GlobalStyles} from './theme'
+
+const StyledApp = styled.div`
+  color : ${(props) => props.theme.fontColor}
+`;
 
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+          SATHISH
+          <button onClick={themeToggler}>
+            Change theme
+          </button>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
